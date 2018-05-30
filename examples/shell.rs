a380fcdcb83e248199852026a2d1203f5057b176
Curtis Millar <curtis@curtism.me>
2018-05-30 23:09:12 +1000

    Add mount namespace to shell example.

--- examples/shell.rs
+++ examples/shell.rs
@@ -8,8 +8,12 @@ use std::process::Command;
 use isolate::*;
 
 fn main() -> isolate::Result<()> {
+	let user_ns = namespace::User::new()
+		.map_root_user()
+		.map_root_group();
 	let context = Context::new()
-		.with(Box::new(namespace::User::new()));
+		.with(user_ns)
+		.with(namespace::Mount::new());
 
 	let child = context.exec(shell)?;
 	child.wait()
